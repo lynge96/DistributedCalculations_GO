c8c34ea0dd4afa4a1162812faeb88c89bdccfc28
Anders Lynge Ravnsbæk <90785699+lynge96@users.noreply.github.com>
2026-03-13 13:35:35 +0100

    authenticator struktur

---
+++ authenticator_api/internal/models/user.go
@@ -0,0 +1,29 @@
+﻿package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID           uuid.UUID `json:"id"`
+	Username     string    `json:"username"`
+	PasswordHash string    `json:"-"`
+	CreatedAt    time.Time `json:"created_at"`
+}
+
+func NewUser(username, password string) (User, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return User{}, err
+	}
+
+	return User{
+		ID:           uuid.New(),
+		Username:     username,
+		PasswordHash: string(hash),
+		CreatedAt:    time.Now(),
+	}, nil
+}
